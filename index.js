4f2c05bb25caaf3df9f6e350d2123ee735d72eb1
hieuchar <hieuvo963@gmail.com>
2017-08-10 08:59:10 -0600

    Added mongooose require

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ var express = require('express'),
 		path = require('path'),
 		pug = require('pug'),
 		bodyParser = require('body-parser'),
-		bcrypt = require('bcrypt-nodejs');
+		bcrypt = require('bcrypt-nodejs'),
+        mongoose = require('mongoose');
