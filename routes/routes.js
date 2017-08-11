051f7e4c29560768da4362bc749297079a75ea7d
Mike Lang <mlang@student.neumont.edu>
2017-08-11 08:02:29 -0600

    - Created the routes folder and routes.js - Moved the mongoose require to routes.js - Added files style.css and layout.pug
    Signed-off-by: Mike Lang <mlang@student.neumont.edu>

---
+++ routes/routes.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/data');
+
+var mdb = mongoose.connection;
+mdb.on('error', console.error.bind(console, 'connection error:'));
+mdb.once('open', function (callback) { // There wasn't anything in here in the demo files
+
+});
+
+var personSchema = mongoose.Schema({ // Put the schema for our data in here
+	
+});
+
+var Person = mongoose.model('People_Collection', personSchema);
+
+exports.route = function (req, res) { // Route template
+  
+};
